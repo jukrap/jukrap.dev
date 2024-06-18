66dd2fa5b0b52a3cd6d6eef84fdc45904898d64f
jukrap <jukrap628@gmail.com>
2024-06-18 18:11:49 +0900

    feat: about page의 skillsSection 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Image from 'next/image';
 import IntroSection from './introSection';
+import SkillsSection from './skillsSection';
 const About: React.FC = () => {
 	return (
-		<div className="flex flex-col items-center px-20 py-20 w-full h-full gap-64">
+		<div className="flex flex-col items-center px-20 py-20 w-full h-full gap-20">
 			<IntroSection />
+			<SkillsSection />
 		</div>
 	);
 };

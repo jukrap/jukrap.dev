59e0863fec91cdf9c549960edf1de86a9b279997
jukrap <jukrap628@gmail.com>
2024-06-26 01:28:54 +0900

    fix: 버그 해결 목적의 명칭 변경

--- src/app/about/projectDetail.tsx
+++ src/app/about/projectDetail.tsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { ProjectDetailType, LinkType } from './types';
 import { useIcon } from '@/hook/useIcon';
-import ImageViewer from '@/components/imageViewer';
-import InfiniteCarousel from '@/components/infiniteCarousel';
+import ImageViewer from '@/components/imageViewer2';
+import InfiniteCarousel from '@/components/infiniteCarousel2';
 
 interface ProjectDetailProps {
 	project: ProjectDetailType;

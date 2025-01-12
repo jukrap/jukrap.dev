6e7736340063861649bca44e7190774495f46e80
jukrap <jukrap628@gmail.com>
2025-01-12 23:07:12 +0900

    rename: 네이밍 컨벤션 오류로 인한 파일명 재작업

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Metadata } from 'next';
 import IntroSection from './introSection';
 import SkillsSection from './skillsSection';
 import ActivitySection from './activitySection';
 import AwardsSection from './awardsSection';
 import SideProjectsSection from './sideProjectsSection';
+
+export const metadata: Metadata = {
+	title: 'About | Ju-cheol Park',
+	description: 'About page of Ju-cheol Park, Frontend Engineer',
+};
 
 const About: React.FC = () => {
 	return (

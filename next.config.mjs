502fefb43df7759e9079fd6eb5616f8fae9a2e5c
jukrap <jukrap628@gmail.com>
2025-01-27 13:58:14 +0900

    fix: 이미지 최적화

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,18 @@
 
 const nextConfig = {
 	reactStrictMode: true,
+	images: {
+		// 이미지 최적화 설정
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+		formats: ['image/webp'],
+		minimumCacheTTL: 60,
+	},
 	webpack: (config) => {
 		config.module.rules.push({
 			test: /\.svg$/,
 			use: ['@svgr/webpack'],
 		});
-
 		return config;
 	},
 };
